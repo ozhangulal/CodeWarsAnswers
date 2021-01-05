a2c54335e6e02529b72f9eb4f328f3553c425876
ozhangulal <ozhangulal@outlook.com>
2021-01-05 21:04:54 +0300

    Multiples of 3 or 5
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    
    Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.

---
+++ CodeWarsMultiplesof3or5/CodeWarsMultiplesof3or5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsMultiplesof3or5
+{
+    class Program
+    {
+        public static int Solution(int value)
+        {
+            return Enumerable.Range(0, value).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(10));
+            Console.ReadLine();
+        }
+    }
+}
